e6518400cd0f594c9616600b0def9df7af6ff408
Tampoka <goal.thanks@gmail.com>
2021-10-21 23:20:10 +0900

    added onChangeHandler for input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import {AutoComplete} from "./AutoCompleteData";
+import {autoCompleteData} from "./data";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <AutoComplete data={autoCompleteData}/>
+        </div>
+    );
 }
 
 export default App;
--- src/AutoCompleteData.tsx
+++ src/AutoCompleteData.tsx
@@ -1,21 +1,34 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import {AutoCompleteDataType} from "./data";
 
 export type AutoCompletePropsType={
     data:AutoCompleteDataType
 }
-const AutoComplete:React.FC<AutoCompletePropsType> = ({data}) => {
+export const AutoComplete:React.FC<AutoCompletePropsType> = ({data}) => {
 
-    const [suggestions, setSuggestions] = useState([]);
-    const [suggestionIndex, setSuggestionIndex] = useState(0);
-    const [suggestionsActive, setSuggestionsActive] = useState(false);
-    const [value, setValue] = useState("");
+    const [suggestions, setSuggestions] = useState<AutoCompleteDataType>([]);
+    const [suggestionIndex, setSuggestionIndex] = useState<number>(0);
+    const [suggestionsActive, setSuggestionsActive] = useState<boolean>(false);
+    const [value, setValue] = useState<string>("");
+
+    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        const query = e.target.value.toLowerCase();
+        setValue(query);
+        if (query.length > 1) {
+            const filterSuggestions = data.filter(
+                (suggestion) => suggestion.toLowerCase().indexOf(query) > -1
+            )
+            setSuggestions(filterSuggestions)
+            setSuggestionsActive(true);
+        } else {
+            setSuggestionsActive(false);
+        }
+    };
 
     return (
-        <div className="autocomplete">
-            <input type="text" />
+        <div className="autocomplete" >
+            <input type="text" onChange={onChangeHandler}/>
         </div>
     );
 };
 
-export default AutoComplete;
