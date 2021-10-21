6e10b17d6c5b42652d7667c7c5f8b6a5bd8aface
Tampoka <goal.thanks@gmail.com>
2021-10-21 23:09:04 +0900

    added useState

--- src/AutoCompleteData.tsx
+++ src/AutoCompleteData.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import {AutoCompleteDataType} from "./data";
 
 export type AutoCompletePropsType={
     data:AutoCompleteDataType
 }
 const AutoComplete:React.FC<AutoCompletePropsType> = ({data}) => {
+
+    const [suggestions, setSuggestions] = useState([]);
+    const [suggestionIndex, setSuggestionIndex] = useState(0);
+    const [suggestionsActive, setSuggestionsActive] = useState(false);
+    const [value, setValue] = useState("");
+
     return (
         <div className="autocomplete">
             <input type="text" />
