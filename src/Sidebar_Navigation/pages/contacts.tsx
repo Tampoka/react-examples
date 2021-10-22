4022d470cdd0cc699c8acef94832389496369dd1
Tampoka <goal.thanks@gmail.com>
2021-10-22 23:06:22 +0900

    got folder structure

---
+++ src/Sidebar_Navigation/pages/contacts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contacts() {
+    return (
+        <div className="page">
+            <h1>Contacts</h1>
+            <p>Here are our contacts:</p>
+        </div>
+    );
+}
+
+export default Contacts;
---
+++ src/Sidebar_Navigation/pages/services.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Services() {
+    return (
+        <div className="page">
+            <h1>Services</h1>
+            <p>This is the page describing our services</p>
+        </div>
+    );
+}
+
+export default Services;
